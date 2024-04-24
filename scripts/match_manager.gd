5505485f2c0e927bb1b286623ab88bf65bd0685a
Nicolás Santibáñez <nsantibanezb@gmail.com>
2024-04-23 21:41:52 -0400

    wip: ending points in `MatchManager` & communication with `TeamManager`

--- scripts/match_manager.gd
+++ scripts/match_manager.gd
@@ -1,49 +1,47 @@
 extends Node3D
 
+signal point_ended(winning_team: TeamManager)
+signal game_ended()
+signal set_ended()
+signal match_ended()
+signal serve_ended()
+
 const ball_scene: Resource = preload ("res://scenes/ball.tscn")
 const POINTS = {
 	0: 0,
 	1: 15,
 	2: 30,
-	3: 40
+	3: 40,
+	4: 'ADV'
 }
 
 @export var player1: Player
 @export var player2: Player
 
-var team_to_serve
-
-var team1: Dictionary = {
-	"points" = 0,
-	"games" = 0,
-	"sets" = 0,
-	"players" = [],
-	"hit_direction" = -1
-}
-
-var team2: Dictionary = {
-	"points" = 0,
-	"games" = 0,
-	"sets" = 0,
-	"players" = [],
-	"hit_direction" = 1
-}
-
-var serving_team = team1
+var teams = []
+var serving_team: TeamManager
+var current_turn_index: int
 
 var balls_in_game = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_load_teams()
 	if randf() <= 0.5:
-		team_to_serve = team1
-	player1.ball_hit.connect(_on_player_ball_hit)
-	player2.ball_hit.connect(_on_player_ball_hit)
-	team1.get("players").append(player1)
-	team2.get("players").append(player2)
+		serving_team = teams[0]
+	else:
+		serving_team = teams[1]
+	for team in teams:
+		# Team's signals connections
+		team.ball_hit.connect(_on_team_ball_hit)
+		# Connect own signals to Teams
+		self.point_ended.connect(team._on_point_ended)
+		self.game_ended.connect(team._on_game_ended)
+		self.set_ended.connect(team._on_set_ended)
+		self.serve_ended.connect(team._on_serve_ended)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	var children = get_parent().get_children()
 	for child in children:
 		if is_instance_of(child, Ball):
@@ -51,27 +49,53 @@ func _process(delta):
 			if not balls_in_game.has(ball_index):
 				balls_in_game[ball_index] = child
 				child.double_bounce.connect(_on_ball_double_bounce)
+	if teams.length() < 2:
+		print("ERROR: Need at least to teams to have a valid match")
 
-func end_point(_winning_team: Dictionary):
+func _load_teams():
+	for co_node in get_parent().get_children():
+		if is_instance_of(co_node, TeamManager):
+			teams.append(co_node)
+			co_node.points = 0
+
+func go_to_point_ended_state():
+	var winning_index = (current_turn_index + 1) % 2
 	# End point logic
 	# 1. Tell players that the point has ended
-	for i in len(team1.get("players")):
-		team1.get("players")[i].change_to_point_ended_state(true)
-		team2.get("players")[i].change_to_point_ended_state(true)
+	# Gana el jugador a quién no le tocaba, cuando el punto terminó
+	var winning_team = teams[winning_index]
+	var losing_team = teams[current_turn_index]
+	point_ended.emit(winning_team)
 	# 2. Show point replay
 	# TODO: POINT REPLAY
-	# 3. Check if game point
-	# 3.	true -> change to game point state
-	# 			TODO: END CURRENT GAME
-	# 3.	false -> change to serve state
-	# 			TODO: TELL PLAYERS to switch to serve state
-	go_to_serve_state()
+	# 3. Add point
+
+	# 4. Check if game point
+	if last_game_point_played(winning_team, losing_team):
+		# TODO: END CURRENT GAME
+		go_to_game_ended_state()
+	else:
+		winning_team.add_point()
+		# TODO: TELL PLAYERS to switch to serve state
+		go_to_serve_state()
+
+	# 4.	true -> change to game point state
+	# 4.	false -> change to serve state
+
+func go_to_game_ended_state():
+	pass
 
 func go_to_serve_state():
-	for i in len(team1.get("players")):
-		team1.get("players")[i].change_to_serve_state(true)
-		team2.get("players")[i].change_to_serve_state(true)
-	pass
+	for team in teams:
+		if team == serving_team:
+			team.change_to_serve_state(true)
+		else:
+			team.change_to_serve_state(false)
+
+func last_game_point_played(winning_team: TeamManager, losing_team: TeamManager) -> bool:
+	if winning_team.points == 4 or (winning_team.points == 3 and losing_team.points < 3):
+		return true
+	return false
 
 func _on_ball_double_bounce(ball: Ball):
 	# Add point to correct team
@@ -80,7 +104,11 @@ func _on_ball_double_bounce(ball: Ball):
 	if balls_in_game.has(ball.get_index()):
 		balls_in_game.erase(ball.get_index())
 	ball.queue_free()
-	end_point(team1)
+	# TODO: Avisar quién gano el punto
+	go_to_point_ended_state()
+
+func _on_team_ball_hit(hit_direction: int, hit_angle: float, ball: Ball):
+	_deprecated_copy_ball_on_hit(ball, hit_direction, hit_angle)
 
 func _on_player_ball_hit(player_id: int, hit_angle: float, ball: Ball):
 	var hit_direction: int = -1
--- scripts/team_manager.gd
+++ scripts/team_manager.gd
@@ -2,8 +2,31 @@
 class_name TeamManager
 extends Node3D
 
+signal ball_hit(hit_direction: int, hit_angle: float, ball: Ball)
+
 # Array of Player
-var _players = []
+var points: int = 0
+
+var players = []
+
+var serving_player_index: int = 0
+
+func add_point():
+	points += 1
+
+func _on_point_ended(winning_team: TeamManager):
+	var won = false
+	if winning_team == self:
+		won = true
+	for p in players:
+		p.change_to_point_ended_state(won)
+
+func _on_game_ended():
+	pass
+func _on_set_ended():
+	pass
+func _on_serve_ended():
+	pass
 
 # Use setters to update the configuration warning automatically.
 func _get_configuration_warnings():
@@ -11,9 +34,9 @@ func _get_configuration_warnings():
 
 	for child in get_children():
 		if is_instance_of(child, Player):
-			_players.append(child)
+			players.append(child)
 	
-	if len(_players) == 0:
+	if len(players) == 0:
 		warnings.append("There must be at least one valid Player as child of this node")
 
 	# Returning an empty array means "no warning".
